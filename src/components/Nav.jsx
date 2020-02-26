03d2744236deda0c3ed352d913172389bee1da85
Matthew Doering <mattdoering86@gmail.com>
2020-02-26 05:55:50 -0500

    started website for dimmagios added navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Nav, Navbar, Button, Form, FormControl} from 'react-bootstrap';
+import './nav.css';
+
+const NavigationBar = () => {
+
+  return (
+    <div>
+    <Navbar className="navbar-custom ">
+      <Navbar.Brand href="/">Dimmagios</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link href="">Menus</Nav.Link>
+        <Nav.Link href="">Photos</Nav.Link>
+        <Nav.Link href="">Vist Us</Nav.Link>
+        <Nav.Link href="">News / Reviews</Nav.Link>
+      </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-primary">Search</Button>
+    </Form>
+  </Navbar>
+    </div>
+  )
+}
+
+export default NavigationBar;
---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <div>
+        <p>Zombies reversus ab inferno, nam malum cerebro. De carne animata corpora quaeritis. Summus sit​​, morbo vel maleficia? De Apocalypsi undead dictum mauris. Hi mortuis soulless creaturas, imo monstra adventus vultus comedat cerebella viventium. Qui offenderit rapto, terribilem incessu. The voodoo sacerdos suscitat mortuos comedere carnem. Search for solum oculi eorum defunctis cerebro. Nescio an Undead zombies. Sicut malus movie horror.</p>
+      </div>
+    </div>
+  )
+}
+
+export default About;
